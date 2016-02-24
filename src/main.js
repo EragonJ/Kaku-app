2aa1bdb6f82fda4986183df6f636da381f432a66
EragonJ <eragonj@eragonj.me>
2016-02-24 15:58:29 +0800

    let's put the topRankingComponent inside the tabBar !

--- src/main.js
+++ src/main.js
@@ -1,12 +1,73 @@
 'use strict';
 
-import React, { View, Component } from 'react-native';
+import Styles from './styles';
 import TopRankingComponent from './views/components/topranking';
+import React, { View, Text, Component } from 'react-native';
+import { TabBarIOS } from 'react-native-icons';
+const TabBarItemIOS = TabBarIOS.Item;
 
 class KakuApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedTab: 'home'
+    };
+  }
+
+  _renderContent(word) {
+    return (
+      <View style={Styles.container}>
+        <Text>{word}</Text>
+      </View>
+    );
+  }
+
+  _renderHome() {
+    return (
+      <View style={{flex: 1}}>
+        <TopRankingComponent/>
+      </View>
+    );
+  }
+
   render() {
     return (
-      <TopRankingComponent/>
+      <TabBarIOS
+        selectedTab={this.state.selectedTab}
+        tintColor={'#c1d82f'}
+        barTintColor={'#000000'}
+        style={Styles.tabBar}>
+        <TabBarItemIOS
+          name="home"
+          iconName={'ion|ios-home-outline'}
+          selectedIconName={'ion|ios-home'}
+          title={''}
+          iconSize={32}
+          accessibilityLabel="Home Tab"
+          selected={this.state.selectedTab === 'home'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'home',
+            });
+          }}>
+          {this._renderHome()}
+        </TabBarItemIOS>
+        <TabBarItemIOS
+            name="settings"
+            iconName={'ion|ios-gear-outline'}
+            selectedIconName={'ion|ios-gear'}
+            title={''}
+            iconSize={32}
+            accessibilityLabel="Settings Tab"
+            selected={this.state.selectedTab === 'settings'}
+            onPress={() => {
+            this.setState({
+              selectedTab: 'settings',
+            });
+          }}>
+          {this._renderContent('settings')}
+        </TabBarItemIOS>
+      </TabBarIOS>
     );
   }
 }
---
+++ src/styles.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import { StyleSheet } from 'react-native';
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  tabBar: {
+    backgroundColor: '#dfdfdf',
+    flex: 1,
+    color: '#ff0000',
+    tintColor: '#877324'
+  }
+});
+
+module.exports = Styles;
